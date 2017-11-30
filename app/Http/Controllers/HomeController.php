2f5008c655cf553ef9a123d878aef8c4d6f72db8
webdev522 <webdev522@gmail.com>
2017-11-30 16:54:54 +0800

    ticker table

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use AndreasGlaser\PPC\PPC;
+use WebSocket\Client;
 use App\Coin;
 
 class HomeController extends Controller
@@ -25,47 +25,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $pcc = new PPC();
-        $result = $pcc->getTicker();
-        $coinArray = $result->decoded;
-        foreach($coinArray as $key => $value) {
-            $coin = Coin::where('name', $key)->first();
-            if ($coin === NULL) {
-                $coin = new Coin;
-                $coin->name = $key;
-            }
+        $coins = Coin::all();
+        $name = 'zhu';
 
-            $coin->id = $value['id'];
-            $coin->last = $value['last'];
-            $coin->lowestAsk = $value['lowestAsk'];
-            $coin->highestBid = $value['highestBid'];
-            $coin->percentChange = $value['percentChange'];
-            $coin->baseVolume = $value['baseVolume'];
-            $coin->quoteVolume = $value['quoteVolume'];
-            $coin->isFrozen = $value['isFrozen'];
-            $coin->high24hr = $value['high24hr'];
-            $coin->low24hr = $value['low24hr'];
-            $coin->save();
-            
-        }
-        // print_r(($result->decoded));
-        
-        // error_log('ok');
-
-
-        // return view('home');
-
-        // $array = array(
-        //     'fruit1' => 'apple',
-        //     'fruit2' => 'orange',
-        //     'fruit3' => 'grape',
-        //     'fruit4' => 'apple',
-        //     'fruit5' => 'apple');
-        // foreach($array as $key => $value)
-        // {
-            
-        // }
-
-        // print_r($array);
+        return view('home')->with('coins', $coins);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,47 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-12">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
                     @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success">
+                        {{ session('status') }}
+                    </div>
                     @endif
-
-                    You are logged in!
+                    <table class="table-striped table-bordered table-hover">
+                        <thead>
+                            <tr>
+                                <th>name</th>
+                                <th>id</th>
+                                <th>last</th>
+                                <th>lowestAsk</th>
+                                <th>highestBid</th>
+                                <th>baseVolume</th>
+                                <th>quoteVolume</th>
+                                <th>high24hr</th>
+                                <th>low24hr</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        @foreach ($coins as $coin)
+                            <tr>
+                                <th>{{ $coin->name }}</th>
+                                <th>{{ $coin->id }}</th>
+                                <th>{{ number_format($coin->last, 10) }}</th>
+                                <th>{{ number_format($coin->lowestAsk, 10) }}</th>
+                                <th>{{ number_format($coin->highestBid, 10) }}</th>
+                                <th>{{ number_format($coin->baseVolume, 10) }}</th>
+                                <th>{{ number_format($coin->quoteVolume, 10) }}</th>
+                                <th>{{ number_format($coin->high24hr, 10) }}</th>
+                                <th>{{ number_format($coin->low24hr, 10) }}</th>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                    
                 </div>
             </div>
         </div>
